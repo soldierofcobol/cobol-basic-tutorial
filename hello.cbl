4acd964bd6ec9e5bb5e9985c1e9a79615b01fb22
Soldier of COBOL <77137395+soldierofcobol@users.noreply.github.com>
2021-05-16 18:39:29 +0200

    The very first COBOL program
    1. Compile...
    
    % cobc -x hello.cbl
    
    2. Execute...
    
    % ./hello
    Hello COBOL

---
+++ hello.cbl
@@ -0,0 +1,16 @@
+....+.*..1....+....2....+....3....+....4....+....5....+....6....+....7..
+      *A   B
+
+       id division.
+          program-id. hello.
+          author. Soldier of COBOL.
+
+       environment division.
+       
+       data division.
+       
+       procedure division.
+           display 'Hello COBOL'
+           stop run.
+
+....+.*..1....+....2....+....3....+....4....+....5....+....6....+....7..
